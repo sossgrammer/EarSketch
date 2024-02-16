e41245f01a173d8625b64559776a27359339bf7d
sossgrammer <142520725+sossgrammer@users.noreply.github.com>
2024-02-16 11:21:16 -0500

    Update winSound.py

--- winSound.py
+++ winSound.py
@@ -1,5 +1,9 @@
 # description: 
 from earsketch import *
+win = EIGHT_BIT_ATARI_SFX_011
+win_str = "0++++++++++++++++++++++++++"
 
-EIGHT_BIT_ATARI_SFX_012
+for measure in range(1,2):
+    makeBeat(win, 1, measure, win_str)
+
 setTempo(120)
