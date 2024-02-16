38edfa789b4019cefb243cf9a9c3da69f60f67d1
sossgrammer <142520725+sossgrammer@users.noreply.github.com>
2024-02-16 11:20:48 -0500

    Update deathSound.py

--- deathSound.py
+++ deathSound.py
@@ -1,5 +1,10 @@
 # description: 
 from earsketch import *
-EIGHT_BIT_ATARI_SFX_013
+from earsketch import *
+death = EIGHT_BIT_ATARI_SFX_009
+death_str = "00000+++++++"
+
+for measure in range(1,2):
+    makeBeat(death, 1, measure, death_str)
 
 setTempo(120)
