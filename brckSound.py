35be81ffbcfda306c57580f861e775c6f0ec1406
sossgrammer <142520725+sossgrammer@users.noreply.github.com>
2024-02-16 11:20:30 -0500

    Update brckSound.py

--- brckSound.py
+++ brckSound.py
@@ -1,5 +1,9 @@
 # description: 
 from earsketch import *
+brick = EIGHT_BIT_ATARI_SFX_010
+brick_str = "0++"
 
+for measure in range(1,2):
+    makeBeat(brick, 1, measure, brick_str)
 
 setTempo(120)
